ac8cc3477255c00b2f42d451a8b03828463b7e24
owororo <ter_r_rarium@mail.ru>
2021-12-06 13:46:45 +0300

    regular expressions 06.12

---
+++ firstapp/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return HttpResponse("<h2>Главная</h2>")
+def about(request):
+    return HttpResponse("<h2>О сайте</h2>")
+def contact(request):
+    return HttpResponse("<h2>Контакты</h2>")
